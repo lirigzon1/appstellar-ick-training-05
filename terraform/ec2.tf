1f342df367fdd5a4f3068edd270321ca7a36e317
Lirigzon Salihu <lsalih@3cis.net>
2023-07-19 19:08:27 +0200

    assignment

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -6,6 +6,6 @@ subnet_id = "subnet-03ddf0d4fd589ae9a"
 security_groups = ["sg-0dd3a367880737f7a"]
 type = "t2.micro"
 device_index = 0
-instance_name = "Lirigzon-Devopsi"
+instance_name = "Lirigzon"
 environment = "Dev"
 }
