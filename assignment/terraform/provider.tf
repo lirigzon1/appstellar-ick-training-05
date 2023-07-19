a6d6ba81a84496597ecd7f4c5fb7a27b7ccb53bb
Lirigzon Salihu <lsalih@3cis.net>
2023-07-19 19:20:33 +0200

    Assignment

--- assignment/terraform/provider.tf
+++ assignment/terraform/provider.tf
@@ -5,3 +5,10 @@ provider "aws" {
   access_key =var.aws_access_key
   secret_key = var.aws_secret_key
 }
+
+provider "aws" {
+  alias = "us_east_1"
+  region = "us_east_1"
+  access_key =var.aws_access_key
+  secret_key = var.aws_secret_key
+}
--- assignment/terraform/variables.tf
+++ assignment/terraform/variables.tf
@@ -1,7 +1,8 @@
-variable access_key {
+variable aws_access_key {
   type        = string
-  
+  description = "AWS Access key"
 }
-variable secret_key {
-    
+variable aws_secret_key {
+     type        = string
+     description = "AWS Secret key"
 }
