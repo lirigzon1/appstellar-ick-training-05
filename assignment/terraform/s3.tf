4f4827d9e9fce565dff744f1f3e3e3b7467cdf3d
Lirigzon Salihu <lsalih@3cis.net>
2023-07-19 19:35:32 +0200

    s

--- assignment/terraform/s3.tf
+++ assignment/terraform/s3.tf
@@ -1,5 +1,5 @@
 module "web_client"{
     source = "terraform-aws-modules/s3-bucket/aws"
     version = "3.14.0"
-    bucket = "Lirigzon-Assignment-4-workspace-group5"
+    bucket = "assignment-4-workspace-lirigzon"
 }
